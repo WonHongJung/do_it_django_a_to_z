ae69923adf7fc99a1c82a689fc1eaa7a2c04c5d7
WonHong Jung <dhdnfka2@naver.com>
2022-12-01 12:03:06 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일하기

--- blog/tests.py
+++ blog/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
 
-class testView(TestCase):
+class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
