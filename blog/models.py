e588883a80cd07a6af9fa51f1053392c70f944f7
WonHong Jung <dhdnfka2@naver.com>
2022-12-06 16:59:12 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -82,3 +82,9 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1371/40a61729327074e2/svg/{self.author.email}'
+
